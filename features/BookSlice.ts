2b474f5400618e4514436dafecd3706879956782
hazelilla <elanur-1998@live.com>
2023-09-25 14:04:00 +0200

    feature: Remove Book button and Empty Shelf button

--- features/BookSlice.ts
+++ features/BookSlice.ts
@@ -14,12 +14,29 @@ export const bookSlice = createSlice({
     reducers: {
         addBook: (state, action) => {
             state.books = [...state.books, action.payload]
-        }
+        },
+
+        removeBook: (state, action) => {
+            const index = state.books.findIndex((book) => book.code === action.payload.code);
+
+            let newShelf = [...state.books];
+            if (index >= 0) {
+                newShelf.splice(index, 1)
+            } else {
+                console.warn('cant remove product with id:', action.payload.code);
+            } 
+            state.books = newShelf;
+        },
+
+        emptyShelf: (state, action) => {
+            state.books = state.books.filter((item) => item.code !== action.payload.code);
+            state.books = [];
+        }   
     }
 })
 
 
-export const {addBook} = bookSlice.actions
-export const getBook = (state: any, id:any) => state.bookStore.books.find((item: { id: any; }) => item.id === id);
+export const {addBook, removeBook, emptyShelf} = bookSlice.actions
+export const getBook = (state: any, code:any) => state.bookStore.books.find((item: { code: any; }) => item.code === code);
 export const getBooks = (state: any) => state.books.books;
 export default bookSlice.reducer;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -8,10 +8,10 @@ import {
   Image,
   FlatList
 } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import BookCard from '../components/BookCard';
 import { ScreenProp } from '../types';
-import { getBooks } from '../features/BookSlice';
+import { getBooks, removeBook, emptyShelf } from '../features/BookSlice';
 import Icon from "react-native-vector-icons/Octicons";
 
 interface Book {
@@ -24,6 +24,17 @@ interface Book {
 const HomeScreen = ({ navigation }: ScreenProp) => {
 
   const books: Book[] = useSelector((state) => getBooks(state));
+
+  const dispatch = useDispatch();
+
+  const removeBookFromShelf = (code: string) => {
+    dispatch(removeBook({ code }))
+  }
+
+  const removeAllBooksFromShelf = () => {
+    dispatch(emptyShelf({}))
+  }
+  
 
   return (
     <SafeAreaView style={{ display: 'flex', flex: 1, backgroundColor: 'beige' }}>
@@ -43,7 +54,7 @@ const HomeScreen = ({ navigation }: ScreenProp) => {
             </TouchableOpacity>
           </View>
 
-          {/* Add book button */}
+          {/* Add Book Button */}
           <TouchableOpacity
             style={styles.button}
             onPress={() => navigation.navigate('FormScreen')}>
@@ -52,6 +63,15 @@ const HomeScreen = ({ navigation }: ScreenProp) => {
             </Text>
           </TouchableOpacity>
 
+          {/* Empty Shelf Button */}
+          {books.length > 1 && (
+          <TouchableOpacity onPress={removeAllBooksFromShelf}>
+            <Text style={styles.emptyShelf}>
+              EMPTY SHELF
+            </Text>
+          </TouchableOpacity>
+          )}
+
           {/* Book Cards */}
           <FlatList
             data={books}
@@ -59,8 +79,8 @@ const HomeScreen = ({ navigation }: ScreenProp) => {
             renderItem={({ item }) => (
               <View>
 
-                {/* delete button */}
-                <TouchableOpacity>
+                {/* Delete Button */}
+                <TouchableOpacity onPress={() => removeBookFromShelf(item.code)}>
                   <Icon name="repo-deleted" size={35} color="black" style={{marginBottom: 5}} />
                 </TouchableOpacity>
               
@@ -100,6 +120,15 @@ const styles = StyleSheet.create({
     borderColor: 'darkkhaki',
     margin: 20,
   },
+  emptyShelf: {
+    fontSize: 20, 
+    borderBottomWidth: 1, 
+    borderRightWidth: 1, 
+    borderLeftWidth:1, 
+    borderColor: 'gray', 
+    paddingHorizontal: 10,
+    marginBottom: 15
+  }
 });
 
 export default HomeScreen;
